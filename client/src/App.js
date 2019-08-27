0be4ce70e7951495c96ccb334eb75a058b05c6c9
Jesús Camargo R <acjesus@uninorte.edu.co>
2019-08-27 13:55:49 -0500

    feat: switch button for identify as a professor or student

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,20 @@
 import React from 'react';
 //import logo from './logo.svg';
 import './App.css';
-//import GoogleLogin from 'react-google-login';
-//import GoogleLogout from 'react-google-login';
 import { QRCode } from "react-qr-svg";
-//import QrCodeScanner from '@sensorfactdev/qr-code-scanner';
 
 import QrRegister from './components/QrRegister';
 import GoogleButton from './components/GoogleButton';
+import ProfessorSwicth from './components/ProfessorSwitch';
 
-/*const handleScanResult = result => {
-  console.log(result);
-  // {
-  //   "result": "RESULT STRING VALUE",
-  //   "points": [
-  //     {
-  //       "x": 171,
-  //       "y": 445,
-  //       "count": 2,
-  //       "estimatedModuleSize": 3.857142857142857
-  //     },
-  //     // .... more points
-  //   ]
-  // }
-}*/
-
-/*var res;
-const result = (data) => {
-  res = data;
-}*/
-
-/*console.log({qr_png,png_string})
-alert(png_string)*/
 function App() {
   return (
     <div className="App">
       <div className="log">
         <header className="App-header">
           <GoogleButton/>
-          </header>
+          <ProfessorSwicth/>
+        </header>
       </div>
       <div className="content">
           <QRCode
@@ -54,5 +30,5 @@ function App() {
     </div>
   );
 }
-//console.log(test)
+
 export default App;
